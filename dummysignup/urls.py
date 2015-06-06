9a8df23a29ca18f17f98c285b86e6e5b148acdd7
Emmanuel Lodovice <name3anad@gmail.com>
2015-06-06 08:49:36 +0800

    setup application

--- dummysignup/urls.py
+++ dummysignup/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from views import Home
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'dummysignup.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', Home.as_view(), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
